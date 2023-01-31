f7703211b2d6c58bb84c53e974de6fc62f793ac9
Thomas Grønvold <thomasg@getacademy.no>
2023-01-31 14:33:46 +0100

    Uke3 changes

--- mini_Intro_Til_Loops_Av_Array/opp_2_shopping_cart/index.js
+++ mini_Intro_Til_Loops_Av_Array/opp_2_shopping_cart/index.js
@@ -1,31 +1,58 @@
 const shoppingList = document.getElementById("shoppingList");
 const inputId = document.getElementById("inputId");
-let shoppingItems = ["test", "test1", "test2"];
+let shoppingItems = ["test", "test1", "test2", "test1"];
 
 updateCart();
 function updateCart() {
   if (shoppingItems.length === 0) {
     shoppingList.innerHTML = "You have no items in your shopping cart.";
   } else {
-    shoppingList.innerHTML = "";
-    /* Here we generate <li>(list item) for every item in the var shoppingIteem */
-    for (i = 1; i <= shoppingItems.length; i++) {
-      if (shoppingList.innerHTML.includes(shoppingItems[i])) {
-        console.log("hello");
+    shoppingItems.forEach(function name(shoppingItem, i) {
+      if (shoppingList.innerHTML.includes(shoppingItem)) {
+        // console.log(shoppingItem);
+        // console.log(shoppingList.innerHTML);
+      } else {
+        shoppingList.innerHTML += /* HTML */ `<li id="listItem1">
+          <h3 id="itemText${i}">${shoppingItems[i]}</h3>
+          <button class="removeButton" onclick="(removeItem(${i}))">❌</button>
+          <button class="removeButton" onclick="(checkMarkButton(${i}))">
+            ✔
+          </button>
+        </li>`;
       }
-      shoppingList.innerHTML += /* HTML */ `<li id="listItem1">
-        <h3 id="itemText${i}">${shoppingItems[i - 1]}</h3>
-        <button class="removeButton" onclick="(removeItem(${i}))">❌</button>
-        <button class="removeButton" onclick="(checkMarkButton(${i}))">✔</button>
-      </li>`;
-      /* Every list item gets their own button to remove that specific item*/
-      // console.log(shoppingList.innerHTML);
-    }
+    });
   }
 }
 
+// updateCart();
+// function updateCart() {
+//   if (shoppingItems.length === 0) {
+//     shoppingList.innerHTML = "You have no items in your shopping cart.";
+//   } else {
+//     shoppingList.innerHTML = "";
+//     /* Here we generate <li>(list item) for every item in the var shoppingIteem */
+//     for (i = 1; i <= shoppingItems.length; i++) {
+//       /* Only add new items, and skip items already rendered on the page */
+//       if (shoppingList.innerHTML.includes(shoppingItems)) {
+//       } else {
+//         shoppingList.innerHTML += /* HTML */ `<li id="listItem1">
+//           <h3 id="itemText${i}">${shoppingItems[i - 1]}</h3>
+//           <button class="removeButton" onclick="(removeItem(${i}))">❌</button>
+//           <button class="removeButton" onclick="(checkMarkButton(${i}))">
+//             ✔
+//           </button>
+//         </li>`;
+/* Every list item gets their own button to remove that specific item*/
+// console.log(shoppingList.innerHTML);
+//       }
+//     }
+//   }
+// }
+
 function removeItem(liIndex) {
-  shoppingItems.splice(liIndex - 1, 1);
+  shoppingItems.splice(liIndex, 1);
+  console.log(liIndex - 1)
+  console.log(shoppingItems);
   updateCart();
 }
 
